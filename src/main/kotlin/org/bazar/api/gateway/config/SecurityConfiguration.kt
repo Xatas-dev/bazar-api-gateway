1c325dec5ec25e22723477fb55eb93263842b3e8
Arseniy <khudyakov.03@bk.ru>
2026-01-07 21:42:17 +0300

    Init commit - added Oauth2 authentication - added bazar-space - added bazar-persona

---
+++ src/main/kotlin/org/bazar/api/gateway/config/SecurityConfiguration.kt
@@ -0,0 +1,44 @@
+package org.bazar.api.gateway.config
+
+import org.springframework.cloud.gateway.route.RouteLocator
+import org.springframework.cloud.gateway.route.builder.PredicateSpec
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpStatus
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
+import org.springframework.security.config.web.server.ServerHttpSecurity
+import org.springframework.security.web.authentication.HttpStatusEntryPoint
+import org.springframework.security.web.server.SecurityWebFilterChain
+import org.springframework.security.web.server.authentication.HttpStatusServerEntryPoint
+import org.springframework.security.web.server.authentication.RedirectServerAuthenticationSuccessHandler
+import org.springframework.security.web.server.context.NoOpServerSecurityContextRepository
+import java.util.function.Function
+
+
+@Configuration
+@EnableWebFluxSecurity
+class SecurityConfiguration {
+
+    @Bean
+    fun springSecurityFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
+        return http
+            .csrf { it.disable() }
+            .authorizeExchange { exchanges ->
+                exchanges.anyExchange().authenticated()
+            }
+            .oauth2Client { }
+            .oauth2Login {
+                it.authenticationSuccessHandler(
+                    RedirectServerAuthenticationSuccessHandler("http://localhost:5173")
+                )
+            }
+            // Add this block to handle exceptions
+            .exceptionHandling { handling ->
+                handling.authenticationEntryPoint(
+                    HttpStatusServerEntryPoint(HttpStatus.UNAUTHORIZED)
+                )
+            }
+            .build()
+    }
+}
---
+++ src/test/kotlin/org/bazar/api/gateway/ApiGatewayApplicationTests.kt
@@ -0,0 +1,15 @@
+package org.bazar.api.gateway
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.bean.override.mockito.MockitoBean
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
